a95aab555dcbfe83239dc2d87ecdc85784cb9d66
luigi2001 <luigitanga@virgilio.it>
2021-07-08 16:35:36 +0200

    init header

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+      <img class="p-10" src="https://images-eu.ssl-images-amazon.com/images/I/51rttY7a%2B9L.png" alt="logo spotfy">
+  </header>
+</template>
+
+<script>
+export default {
+   name: 'Header'
+}
+</script>
+
+<style lang="scss" scoped>
+header{
+    height: 100px;
+    background-color: black;
+
+    img{
+        width: 80px;
+    }
+}
+</style>
