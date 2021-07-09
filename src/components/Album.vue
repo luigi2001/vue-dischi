9e3b401168b154a145472aee6ffc08e6b6289542
luigi2001 <luigitanga@virgilio.it>
2021-07-09 15:11:44 +0200

    init album

--- src/components/Album.vue
+++ src/components/Album.vue
@@ -1,20 +1,42 @@
 <template>
 <section>
     <div class="container">
-        <div class="row col-5">
-            <div class="box m-3"></div>
-            <div class="box m-3"></div>
-            <div class="box m-3"></div>
-            <div class="box m-3"></div>
-            <div class="box m-3"></div>
+        <div class="row">
+            <div v-for="(cover,index) in arrayDischi" :key="index" class="box m-3 col-2">
+                <Disco :dettagli="cover"/>
+            </div>
         </div>
     </div>
 </section>
 </template>
 
 <script>
+import axios from 'axios';
+import Disco from '@/components/Disco.vue'
 export default {
-
+    name: 'Album',
+    components:{
+        Disco
+    },
+    data(){
+        return {
+            apiUrl: 'https://flynn.boolean.careers/exercises/api/array/music',
+            arrayDischi: ''
+        }
+    },
+    created(){
+        this.dischi();
+    },
+    methods:{
+        dischi(){
+            axios
+                .get(this.apiUrl)
+                .then(risposta => {
+                   this.arrayDischi = risposta.data.response;
+                   console.log(this.arrayDischi);
+                })
+        }
+    }
 }
 </script>
 
@@ -22,10 +44,8 @@ export default {
 section{
     height: calc(100vh - 80px);
     background-color: #1E2D3B;
-
     .box{
-        height: 50px;
-        background-color: red;
+        height: 300px;
     }
 }
 </style>
---
+++ src/components/Disco.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="m-5">
+      <img :src="dettagli.poster" :alt="dettagli.title">
+      <h3>{{dettagli.title}}</h3>
+      <p>
+          {{dettagli.author}}
+          {{dettagli.year}}
+      </p>
+  </section>
+</template>
+
+<script>
+export default {
+    name: 'Disco',
+    props:["dettagli"]
+}
+</script>
+
+<style lang="scss">
+img{
+    width: 200px;
+}
+
+</style>
